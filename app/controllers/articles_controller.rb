4812f665d353b938c76d7e04fe7b386a387c8be3
Rodney Caneda <arekstrie.burnbright@gmail.com>
2020-08-12 11:30:27 +0800

    complete crud for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
-  def show
-    @article = Article.find(params[:id])
-  end
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
+  end
+
+  def show
   end
 
   def new 
@@ -12,11 +13,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article created successfully"
       redirect_to @article
@@ -25,10 +25,8 @@ class ArticlesController < ApplicationController
     end
   end
 
-
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully"
       redirect_to @article
     else
@@ -37,4 +35,19 @@ class ArticlesController < ApplicationController
 
   end
   
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private 
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
