520eff075f34a82ebb54f81cd60fdb443aea63d1
Rodney Caneda <arekstrie.burnbright@gmail.com>
2020-08-19 23:58:13 +0800

    add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]      
+  end
+
+  #Returns true if current user is set
+  def logged_in?
+    !!current_user
+  end
+  
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must login to perform that action"
+      redirect_to login_path
+    end
+  end
+
 end
